1cbfeed5504d8574586a33e75e9b5f14350aeb20
z17 CX <mail@z17.cx>
2022-05-14 21:52:12 +0300

    2022-05-14 18:52:12

--- wim.build.ps1
+++ wim.build.ps1
@@ -141,9 +141,9 @@ function New-BuildImage() {
     # Create Windows image archive.
     Write-BuildMsg -Message "--- Create Windows Image Archive..."
     if ( ! ( Test-Path -Path "$($d_wim)\install.custom.esd" -PathType Leaf ) ) {
-      $($d_app)\7z\7za.exe a -t7z "$($d_wim)\install.custom.esd" "$($d_wim)\install.custom.esd.7z"
+      "$($d_app)\7z\7za.exe" a -t7z "$($d_wim)\install.custom.esd" "$($d_wim)\install.custom.esd.7z"
     } elseif ( ! ( Test-Path -Path "$($d_wim)\install.custom.wim" -PathType Leaf ) ) {
-      $($d_app)\7z\7za.exe a -t7z "$($d_wim)\install.custom.wim" "$($d_wim)\install.custom.wim.7z"
+      "$($d_app)\7z\7za.exe" a -t7z "$($d_wim)\install.custom.wim" "$($d_wim)\install.custom.wim.7z"
     } else {
       Write-Host "Not Found: 'install.custom.esd' or 'install.custom.wim'."
     }

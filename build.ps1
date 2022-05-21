a0f30cd20238854e69be8aac5117a65e1b119bb7
Kitsune Solar <kitsune.solar@gmail.com>
2022-05-21 20:03:05 +0300

    2022-05-21 17:03:05

--- build.ps1
+++ build.ps1
@@ -48,15 +48,6 @@ Param(
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function Start-Build() {
-  # Run.
-  New-Build
-}
-
-# -------------------------------------------------------------------------------------------------------------------- #
-# BUILD IMAGE.
-# -------------------------------------------------------------------------------------------------------------------- #
-
-function New-Build() {
   # Directories.
   $d_app = "$($PSScriptRoot)\Apps"
   $d_drv = "$($PSScriptRoot)\Drivers"
@@ -76,6 +67,15 @@ function New-Build() {
   # Sleep time.
   [int]$sleep = 5
 
+  # Run.
+  Start-BuildFarm
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# BUILD IMAGE.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+function Start-BuildImage() {
   # Check directories.
   if ( ! ( Test-Path "$($d_app)" ) ) { New-Item -Path "$($d_app)" -ItemType "Directory" }
   if ( ! ( Test-Path "$($d_drv)" ) ) { New-Item -Path "$($d_drv)" -ItemType "Directory" }
@@ -94,80 +94,80 @@ function New-Build() {
 
     # Get Windows image hash.
     if ( ! $NoWimHash ) {
-      Write-Msg -Title -Message "--- Get Windows Image Hash..."
+      Write-BFMsg -Title -Message "--- Get Windows Image Hash..."
       Get-FileHash "$($d_wim)\$($f_wim_original)" -Algorithm "SHA256" | Format-List
       Start-Sleep -s $sleep
     }
 
     # Get Windows image info.
-    Write-Msg -Title -Message "--- Get Windows Image Info..."
+    Write-BFMsg -Title -Message "--- Get Windows Image Info..."
     Get-WindowsImage -ImagePath "$($d_wim)\$($f_wim_original)" -ScratchDirectory "$($d_tmp)"
     [int]$wim_index = Read-Host "Enter WIM index (Press [ENTER] to EXIT)"
     if ( ! $wim_index ) { break }
 
     # Mount Windows image.
-    Write-Msg -Title -Message "--- Mount Windows Image..."
+    Write-BFMsg -Title -Message "--- Mount Windows Image..."
     Mount-WindowsImage -ImagePath "$($d_wim)\$($f_wim_original)" -Path "$($d_mnt)" -Index $wim_index -CheckIntegrity -ScratchDirectory "$($d_tmp)"
     Start-Sleep -s $sleep
 
     if ( ( $AddPackages ) -and ( ! ( Get-ChildItem "$($d_upd)" | Measure-Object ).Count -eq 0 ) ) {
       # Add packages.
-      Write-Msg -Title -Message "--- Add Windows Packages..."
+      Write-BFMsg -Title -Message "--- Add Windows Packages..."
       Add-WindowsPackage -Path "$($d_mnt)" -PackagePath "$($d_upd)" -IgnoreCheck -ScratchDirectory "$($d_tmp)"
       Start-Sleep -s $sleep
 
       # Get packages.
-      Write-Msg -Title -Message "--- Get Windows Packages..."
+      Write-BFMsg -Title -Message "--- Get Windows Packages..."
       Get-WindowsPackage -Path "$($d_mnt)" -ScratchDirectory "$($d_tmp)"
       Start-Sleep -s $sleep
     }
 
     # Add drivers.
     if ( ( $AddDrivers ) -and ( ! ( Get-ChildItem "$($d_drv)" | Measure-Object ).Count -eq 0 ) ) {
-      Write-Msg -Title -Message "--- Add Windows Drivers..."
+      Write-BFMsg -Title -Message "--- Add Windows Drivers..."
       Add-WindowsDriver -Path "$($d_mnt)" -Driver "$($d_drv)" -Recurse -ScratchDirectory "$($d_tmp)"
       Start-Sleep -s $sleep
     }
 
     # Reset Windows image.
     if ( $ResetBase ) {
-      Write-Msg -Title -Message "--- Reset Windows Image..."
+      Write-BFMsg -Title -Message "--- Reset Windows Image..."
       Repair-WindowsImage -Path "$($d_mnt)" -StartComponentCleanup -ResetBase -ScratchDirectory "$($d_tmp)"
       Start-Sleep -s $sleep
     }
 
     # Scan health Windows image.
     if ( $ScanHealth ) {
-      Write-Msg -Title -Message "--- Scan Health Windows Image..."
+      Write-BFMsg -Title -Message "--- Scan Health Windows Image..."
       Repair-WindowsImage -Path "$($d_mnt)" -ScanHealth -ScratchDirectory "$($d_tmp)"
       Start-Sleep -s $sleep
     }
 
     # Dismount Windows image.
     if ( $SaveImage ) {
-      Write-Msg -Title -Message "--- Save & Dismount Windows Image..."
+      Write-BFMsg -Title -Message "--- Save & Dismount Windows Image..."
       Dismount-WindowsImage -Path "$($d_mnt)" -Save -ScratchDirectory "$($d_tmp)"
       Start-Sleep -s $sleep
     } else {
-      Write-Msg -Title -Message "--- Discard & Dismount Windows Image..."
+      Write-BFMsg -Title -Message "--- Discard & Dismount Windows Image..."
       Dismount-WindowsImage -Path "$($d_mnt)" -Discard -ScratchDirectory "$($d_tmp)"
       Start-Sleep -s $sleep
     }
 
     if ( $ExportToESD ) {
       # Export Windows image to custom ESD format.
-      Write-Msg -Title -Message "--- Export Windows Image to Custom ESD Format..."
+      Write-BFMsg -Title -Message "--- Export Windows Image to Custom ESD Format..."
       Dism /Export-Image /SourceImageFile:"$($d_wim)\$($f_wim_original)" /SourceIndex:$wim_index /DestinationImageFile:"$($d_wim)\$($f_wim_custom).esd" /Compress:recovery /CheckIntegrity /ScratchDir:"$($d_tmp)"
       Start-Sleep -s $sleep
     } else {
       # Export Windows image to custom WIM format.
-      Write-Msg -Title -Message "--- Export Windows Image to Custom WIM Format..."
+      Write-BFMsg -Title -Message "--- Export Windows Image to Custom WIM Format..."
       Export-WindowsImage -SourceImagePath "$($d_wim)\$($f_wim_original)" -SourceIndex $wim_index -DestinationImagePath "$($d_wim)\$($f_wim_custom)" -CompressionType "max" -CheckIntegrity -ScratchDirectory "$($d_tmp)"
       Start-Sleep -s $sleep
     }
 
     # Create Windows image archive.
-    Write-Msg -Title -Message "--- Create Windows Image Archive..."
+    Write-BFMsg -Title -Message "--- Create Windows Image Archive..."
     if ( Test-Path -Path "$($d_wim)\$($f_wim_custom).esd" -PathType "Leaf" ) {
       New-7z -App "$($d_app)\7z\7za.exe" -In "$($d_wim)\$($f_wim_custom).esd" -Out "$($d_wim)\$($f_wim_custom).esd.7z"
     } elseif ( Test-Path -Path "$($d_wim)\$($f_wim_custom)" -PathType "Leaf" ) {
@@ -186,7 +186,7 @@ function New-Build() {
 # ------------------------------------------------< COMMON FUNCTIONS >------------------------------------------------ #
 # -------------------------------------------------------------------------------------------------------------------- #
 
-function Write-Msg() {
+function Write-BFMsg() {
   param (
     [string]$Message,
     [switch]$Title = $false
@@ -212,4 +212,4 @@ function New-7z() {
 # -------------------------------------------------< INIT FUNCTIONS >------------------------------------------------- #
 # -------------------------------------------------------------------------------------------------------------------- #
 
-Start-Build
+Start-BuildFarm
